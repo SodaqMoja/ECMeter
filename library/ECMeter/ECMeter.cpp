f58648e59127a6b3246c2eafce31bce4a647c15a
Nick Leijenhorst <nickleijenhorst@gmail.com>
2014-05-25 17:32:00 +0200

    Added library folder, testing

---
+++ library/ECMeter/ECMeter.cpp
@@ -0,0 +1,63 @@
+#include "ECMeter.h"
+#include "../SoftI2CMaster/SoftI2CMaster.h"
+
+double systemVoltage;
+
+void setVoltage(double voltage)
+{
+	systemVoltage = voltage;
+}
+
+double getVoltage()
+{
+	return systemVoltage;
+}
+
+double getChannel2()
+{
+  	i2c_start(ADDR|I2C_WRITE);
+	i2c_write(0xA8);
+
+  	i2c_rep_start(ADDR|I2C_READ);
+  	
+	byte h = i2c_read(false);
+  	byte l = i2c_read(false);
+  	byte r = i2c_read(true);
+  
+    i2c_stop();
+  
+  	long t = h << 8 |  l;
+  	
+	if (t >= 32768) t = 65536l - t;
+  	
+	double v = (double) t * 2.048/32768.0;
+
+	return v;
+}
+
+double getChannel1()
+{
+  	i2c_start(ADDR|I2C_WRITE);
+	i2c_write(0x88);
+
+  	i2c_rep_start(ADDR|I2C_READ);
+  	
+	byte h = i2c_read(false);
+  	byte l = i2c_read(false);
+  	byte r = i2c_read(true);
+  
+    i2c_stop();
+  
+  	long t = h << 8 |  l;
+  	
+	if (t >= 32768) t = 65536l - t;
+  	
+	double v = (double) t * 2.048/32768.0;
+
+	return v;
+}
+
+double getTemperature()
+{
+	return (getChannel2()-0.5) * 100;
+}
---
+++ library/ECMeter/ECMeter.h
@@ -0,0 +1,19 @@
+#ifndef EC_H
+#define EC_H
+
+#define NO_INTERRUPT 1
+#define I2C_TIMEOUT 1000
+
+#define SDA_PORT PORTD
+#define SDA_PIN 5
+#define SCL_PORT PORTD
+#define SCL_PIN 4
+#define ADDR 0xD0
+
+double getVoltage();
+void setVoltage(double voltage);
+double getChannel1();
+double getChannel2();
+double getTemperature();
+
+#endif
